742737d519e366f46e3b291300ada3c5c3b70f47
maudehrmann <maud.ehrmann@gmail.com>
2018-03-30 17:48:26 +0200

    adding module elements

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Config for Pypi."""
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+
+
+DESCRIPTION = "Python module for data sanity check."
+
+setup(
+    name='sanity_check',
+    author='Matteo Romanello, Maud Ehrmann',
+    author_email='matteo.romanello@epfl.ch, maud.ehrmann@epfl.ch',
+    url='https://github.com/impresso/impresso-data-sanitycheck',
+    version=VERSION,
+    packages=find_packages(),
+    long_description=DESCRIPTION, install_requires=['docopt']
+    # install_requires=[]
+)
